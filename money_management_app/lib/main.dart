fccab851fd9d4d3250b4fadd7853f0f8d23662cc
Habeebu Rahman <habeebrh321@gmail.com>
2022-03-14 11:55:53 +0530

    installed hive database

--- money_management_app/lib/main.dart
+++ money_management_app/lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:money_management_app/models/category/category_model.dart';
 import 'package:money_management_app/screens/home/screen_home.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+
+  if (!Hive.isAdapterRegistered(CategoryTypeAdapter().typeId))
+    Hive.registerAdapter(CategoryTypeAdapter());
+
+  if (!Hive.isAdapterRegistered(CategoryModelAdapter().typeId))
+    Hive.registerAdapter(CategoryModelAdapter());
   runApp(const MyApp());
 }
 
@@ -15,7 +26,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:  ScreenHome(),
+      home: ScreenHome(),
     );
   }
 }
